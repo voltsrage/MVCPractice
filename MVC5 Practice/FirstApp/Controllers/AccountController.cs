5159113599d2da39b12199b9115928fdfea224b5
Trendon Cato <electric5388@hotmail.com>
2020-06-28 02:44:48 +0800

    Testing Action Results

---
+++ MVC5 Practice/FirstApp/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string Username,string Password)
+        {
+            if(Username == "admin" && Password == "manager")
+            {
+                return RedirectToAction("Dashboard", "Admin");
+            }
+            else
+            {
+                return RedirectToAction("InvalidLogin");
+            }            
+        }
+
+        public ActionResult InvalidLogin()
+        {
+            return View();
+        }
+    }
+}
--- MVC5 Practice/FirstApp/Controllers/HomeController.cs
+++ MVC5 Practice/FirstApp/Controllers/HomeController.cs
@@ -23,5 +23,65 @@ namespace FirstApp.Controllers
         {
             return View();
         }
+
+        //Using Content Type to get content from a list or db
+        //Use Content when you need to send a specific type of content such as plain text
+        public ActionResult GetSubjectName(string SubId)
+        {
+            var subjects = new[]
+            {
+                new{SubId = "Eng",SubName = "English",PassingGrade=80},
+                new{SubId = "Math",SubName = "Mathematics",PassingGrade=70},
+                new{SubId = "Ch",SubName = "Chinese",PassingGrade=65}
+            };
+
+            string matchSubName = null;
+
+            foreach(var item in subjects)
+            {
+                if(SubId == item.SubId)
+                {
+                    matchSubName = item.SubName;
+                }
+            }
+
+            //return new ContentResult() { Content = matchSubName, ContentType = "text/plain" };
+            return Content(matchSubName, "text/plain");
+        }
+
+        //File Action
+        public ActionResult GetSubTest(string SubId)
+        {
+            string fileName = "~/" + SubId + "Test.pdf";
+            return File(fileName,"application/pdf");
+        }
+
+        public ActionResult GetTestSite(string SubId)
+        {
+            var subjects = new[]
+            {
+                new{SubId = "Engl",SubName = "English",PassingGrade=80},
+                new{SubId = "Math",SubName = "Mathematics",PassingGrade=70},
+                new{SubId = "Ch",SubName = "Chinese",PassingGrade=65}
+            };
+
+            string urlLink = null;
+
+            foreach(var item in subjects)
+            {
+                if(SubId == item.SubId)
+                {
+                    urlLink = "https://tw.search.yahoo.com/search?fr=yfp-search-sb&p=" + item.SubName + "Test";
+                }
+            }
+            if (urlLink == null)
+            {
+                return Content("Invalid Sub Id");
+            }
+            else
+            {
+                return Redirect(urlLink);
+            }            
+        }
     }
 }
