4aba4bf3ecd5b26e8b8d1bc5c711808a0e8ca1ad
Trendon Cato <electric5388@hotmail.com>
2020-06-28 19:55:14 +0800

    Added Subject Model

---
+++ MVC5 Practice/FirstApp/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstApp.Models;
+
+namespace FirstApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product() {SubjectId = "Eng",SubjectName = "English",PassingGrade=80},
+                new Product() {SubjectId = "Math",SubjectName = "Mathematics",PassingGrade=70},
+                new Product() {SubjectId = "Ch",SubjectName = "Chinese",PassingGrade=65}
+            };
+            ViewBag.products = products;
+            return View();
+        }
+
+        public ActionResult Details(string id)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product() {SubjectId = "Eng",SubjectName = "English",PassingGrade=80},
+                new Product() {SubjectId = "Math",SubjectName = "Mathematics",PassingGrade=70},
+                new Product() {SubjectId = "Ch",SubjectName = "Chinese",PassingGrade=65}
+            };
+            Product matchingSubject = null;
+            foreach(var item in products)
+            {
+                if(item.SubjectId == id)
+                {
+                    matchingSubject = item;
+                }
+            }
+            ViewBag.MatchingSubject = matchingSubject;
+
+            return View();
+        }
+    }
+}
---
+++ MVC5 Practice/FirstApp/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstApp.Models
+{
+    public class Product
+    {
+        public string SubjectId { get; set; }
+        public string SubjectName { get; set; }
+        public int PassingGrade { get; set; }
+    }
+}
