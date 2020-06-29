91506c4adf2f6162ec704880c9d77f96b61e4162
Trendon Cato <electric5388@hotmail.com>
2020-06-29 22:08:05 +0800

    Added Create, Edit and Delete Options For Products

--- MVC5 Practice/EFDbFoirstApproachExample/Controllers/ProductsController.cs
+++ MVC5 Practice/EFDbFoirstApproachExample/Controllers/ProductsController.cs
@@ -29,6 +29,9 @@ namespace EFDbFoirstApproachExample.Controllers
 
         public ActionResult Create()
         {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            ViewBag.Categories = db.Categories.ToList();
+            ViewBag.Brands = db.Brands.ToList();
             return View();
         }
 
@@ -39,6 +42,53 @@ namespace EFDbFoirstApproachExample.Controllers
             db.Products.Add(p);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long id)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            ViewBag.Categories = db.Categories.ToList();
+            ViewBag.Brands = db.Brands.ToList();
+            Product existingProduct = db.Products.Where(p => p.ProductID == id).FirstOrDefault();
+
+            return View(existingProduct);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product p)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product existingProduct = db.Products.Where(prod => prod.ProductID == p.ProductID).FirstOrDefault();
+
+            existingProduct.ProductName = p.ProductName;
+            existingProduct.Price = p.Price;
+            existingProduct.DateOfPurchase = p.DateOfPurchase;
+            existingProduct.CategoryID = p.CategoryID;
+            existingProduct.BrandID = p.BrandID;
+            existingProduct.AvailabilityStatus = p.AvailabilityStatus;
+            existingProduct.Active = p.Active;
+
+            db.SaveChanges();
+            return RedirectToAction("Index","Products");
+        }
+
+        public ActionResult Delete(long id)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product deleteProduct = db.Products.Where(p => p.ProductID == id).FirstOrDefault();
+
+            return View(deleteProduct);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Product p,long id)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product deleteProduct = db.Products.Where(prod => prod.ProductID == id).FirstOrDefault();
+
+            db.Products.Remove(deleteProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Products");
         }
     }
 }
