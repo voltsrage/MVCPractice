64861d7a4ce6f04595391f6787a76ad023f3e285
Trendon Cato <electric5388@hotmail.com>
2020-06-30 01:01:51 +0800

    Added Pagination,Navigation and Sorting to Product Page

--- MVC5 Practice/EFDbFoirstApproachExample/Controllers/ProductsController.cs
+++ MVC5 Practice/EFDbFoirstApproachExample/Controllers/ProductsController.cs
@@ -12,11 +12,75 @@ namespace EFDbFoirstApproachExample.Controllers
         
 
         // GET: Products
-        public ActionResult Index(string search = "")
+        public ActionResult Index(string search = "", string SortColumn = "ProductName",string IconClass = "fa-sort-asc",int PageNo = 1)
         {
             EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
             ViewBag.search = search;
             List<Product> products = db.Products.Where(p => p.ProductName.Contains(search)).ToList();
+
+            ViewBag.SortColumn = SortColumn;
+            ViewBag.IconClass = IconClass;
+
+            if(ViewBag.SortColumn == "ProductID")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(p => p.ProductID).ToList();
+                else
+                    products = products.OrderByDescending(p => p.ProductID).ToList();
+            }
+            else if(ViewBag.SortColumn == "ProductName")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(p => p.ProductName).ToList();
+                else
+                    products = products.OrderByDescending(p => p.ProductName).ToList();
+            }
+            else if(ViewBag.SortColumn == "Price")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(p => p.Price).ToList();
+                else
+                    products = products.OrderByDescending(p => p.Price).ToList();
+            }
+            else if (ViewBag.SortColumn == "DateOfPurchase")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(p => p.DateOfPurchase).ToList();
+                else
+                    products = products.OrderByDescending(p => p.DateOfPurchase).ToList();
+            }
+            else if (ViewBag.SortColumn == "AvailabilityStatus")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(p => p.AvailabilityStatus).ToList();
+                else
+                    products = products.OrderByDescending(p => p.AvailabilityStatus).ToList();
+            }
+            else if (ViewBag.SortColumn == "CategoryID")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(p => p.Category.CategoryName).ToList();
+                else
+                    products = products.OrderByDescending(p => p.Category.CategoryName).ToList();
+            }
+            else if (ViewBag.SortColumn == "BrandID")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(p => p.Brand.BrandName).ToList();
+                else
+                    products = products.OrderByDescending(p => p.Brand.BrandName).ToList();
+            }
+
+            //Paging
+
+            int NoOfRecordsPerPage = 5;
+            int NoOfPages = Convert.ToInt32(Math.Ceiling((Convert.ToDouble(products.Count) / Convert.ToDouble(NoOfRecordsPerPage))));
+            int NoOfRecordsToSkip = (PageNo - 1) * NoOfRecordsPerPage;
+
+            ViewBag.PageNo = PageNo;
+            ViewBag.NoOfPages = NoOfPages;
+
+            products = products.Skip(NoOfRecordsToSkip).Take(NoOfRecordsPerPage).ToList();
             return View(products);
         }
 
