565554b902fdc6240ec0e1f43afa22f9e146df6c
Trendon Cato <electric5388@hotmail.com>
2020-07-01 11:33:32 +0800

    Creating Identities

---
+++ MVC5 Practice/EFDbFoirstApproachExample/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+[assembly: OwinStartup(typeof(EFDbFoirstApproachExample.Startup))]
+
+namespace EFDbFoirstApproachExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+        }
+    }
+}
