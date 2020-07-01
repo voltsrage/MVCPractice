79e3106ab3e6ec485a17184d4623166b9d968ba7
Trendon Cato <electric5388@hotmail.com>
2020-07-01 11:51:35 +0800

    Changing to Code first to Install Identities

---
+++ MVC5 Practice/EFDbFoirstApproachExample/Identity/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EFDbFoirstApproachExample.Identity
+{
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ MVC5 Practice/EFDbFoirstApproachExample/Identity/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EFDbFoirstApproachExample.Identity
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public DateTime? Birthday { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
